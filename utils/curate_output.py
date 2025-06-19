9481654b04572ceeb70e083785f17c9b2d5f4091
hajerkr <heajer.karoui@gmail.com>
2025-06-19 15:56:15 +0100

    fix

--- utils/curate_output.py
+++ utils/curate_output.py
@@ -23,6 +23,87 @@ def find_gear_version(analyses, filename):
                 elif 'mrr' in asys.label:
                     return f"{file.gear_info.name}/{file.gear_info.version}"
     return None
+def get_age(session, dicom_header):
+    
+    # -------------------  Get the subject age & matching template  -------------------  #
+    # uploaded demographics the most reliable, after that try to get from dicom header
+    # get the T2w axi dicom acquisition from the session
+    # Should contain the DOB in the dicom header
+    # Some projects may have DOB removed, but may have age at scan in the subject container
+
+    age_source = None
+    age = None
+
+    # Check if age is in custom session info
+    if session.info.get('age_at_scan_months', None) != None:
+        age_source = 'custom_info'
+        print(age, age_source)
+
+    else:
+        age = re.sub('\D', '', dicom_header.info.get('PatientAge', None))
+        # Check for PatientAge in the DICOM header
+        if 'PatientAge' != None :
+            print("No custom demographic age uploaded in session info! Trying PatientAge from dicom...")
+            age_raw = dicom_header.info['PatientAge']
+
+            # Parse age and convert to months
+            unit = age_raw[-1].upper()  # Extract the unit (D = Days, W = Weeks, M = Months, Y = Years)
+            numeric_age = int(re.sub('\D', '', age_raw))  # Remove non-numeric characters
+
+            if unit == 'D':  # Days to months
+                age = numeric_age // 30
+            elif unit == 'W':  # Weeks to months
+                age = numeric_age // 4
+            elif unit == 'M':  # Already in months
+                age = numeric_age
+            elif unit == 'Y':  # Years to months
+                age = numeric_age * 12
+            else:
+                print("Unknown unit for PatientAge. Setting age to NA.")
+                age = 'NA'
+
+            age_source = 'dicom_age'
+
+        # If PatientAge is unavailable or invalid, fallback to PatientBirthDate and SeriesDate
+        dob = dicom_header.info.get('PatientBirthDate', None)
+        series_date = dicom_header.get('SeriesDate', None)
+        
+        if age is None or age == 0:
+            if dob != None and series_date != None:
+                print("Trying DOB from dicom...")
+                print("WARNING: This may be inaccurate if false DOB was entered at time of scanning!")
+                # Calculate age at scan
+                # Calculate the difference in months
+                try:
+                    series_dt = datetime.strptime(series_date, '%Y%m%d')
+                    dob_dt = datetime.strptime(dob, '%Y%m%d')
+
+                    dob_dt = datetime.strptime(dob, '%Y%m%d')
+                    series_date_dt = datetime.strptime(series_date, '%Y%m%d')
+                    age_days = (series_date_dt - dob_dt).days
+
+                    # Convert days to months
+                    age = age_days // 30
+                    age_source = 'dicom_DOB'
+
+                except Exception as e:
+                    print(f"Error parsing dates from dicom: {e}")
+                    age = 'NA'
+                    age_source = 'NA'
+
+                # Adjust if the day in series_dt is earlier than the day in dob_dt
+                if series_dt.day < dob_dt.day:
+                    age = 'NA'
+                    age_source = 'NA'
+
+            else:
+                print("No valid birthdate or series date found in dicom header. Setting age to NA.")
+                age = 'NA'
+                age_source = 'NA'
+
+
+    print(age, age_source)
+    return age, age_source
 
 def demo(context):
 
@@ -55,6 +136,7 @@ def demo(context):
     session_id = input_container.parents['session']
     session_container = context.client.get(session_id)
     session = session_container.reload()
+    session_info = session.info
     session_label = session.label
     log.info(f"session label: {session.label}")
     
@@ -89,6 +171,7 @@ def demo(context):
     # Should contain the DOB in the dicom header
     # Some projects may have DOB removed, but may have age at scan in the subject container
 
+    sex,age,age_source = None,None,None
     for acq in session_container.acquisitions.iter():
         # print(acq.label)
         acq = acq.reload()
@@ -98,65 +181,35 @@ def demo(context):
                 if file_obj['type'] == 'dicom':
                     
                     dicom_header = fw._fw.get_acquisition_file_info(acq.id, file_obj.name)
-                    
+                    age, age_source = get_age(session, dicom_header)
+                    scannerSoftwareVersion = dicom_header.info.get('SoftwareVersions', None)
+
                     try:
-                        sex = dicom_header.info.get("PatientSex",session.info.get('sex_at_birth', "NA"))
-                        dob = dicom_header.info.get('PatientBirthDate', None)
-                        series_date = dicom_header.get('SeriesDate', None)
-                        scannerSoftwareVersion = dicom_header.info.get('SoftwareVersions', None)
+                        sex = session_info.get("sex_at_birth", dicom_header.info.get("PatientSex","NA"))
+                    except:
+                        continue
+                    
 
-                        if session.info.get('age_at_scan_months', 0) != 0:
-                                log.info("Checking session info for age at scan in months...")
-                                age_in_months = float(session.info.get('age_at_scan_months', 0))
-
-
-                        elif dob != None and series_date != None:
-                            # Calculate age at scan
-                            # Calculate the difference in months
-                            series_dt = datetime.strptime(series_date, '%Y%m%d')
-                            dob_dt = datetime.strptime(dob, '%Y%m%d')
-
-                            age_in_months = (series_dt.year - dob_dt.year) * 12 + (series_dt.month - dob_dt.month)
-
-                            # Adjust if the day in series_dt is earlier than the day in dob_dt
-                            if series_dt.day < dob_dt.day:
-                                age_in_months -= 1
-                        
-                        else:
-                            log.info("No DOB in dicom header or age in session info! Trying PatientAge from dicom...")
-                            # Need to drop the 'D' from the age and convert to int
-                            age_in_months = re.sub('\D', '', dicom_header.info.get('PatientAge', "0"))
-                            log.info(f"PatientAge from dicom: {age_in_months} months")
-                            age_in_months = int(age_in_months)
-                        
-                        
-                        if age_in_months <= 0 or age_in_months > 1200:  # negative, 0 or 100 years
-                            age_in_months = 'NA'
-                            raise ValueError(f"Invalid age value: {age_in_months} months")
-                        
-                    except ValueError as e:
-                        log.error(f"Error processing dates: {e}")
-                        raise
     
-    #age_in_months = str(age_in_months) + "M"
     # assign values to lists. 
-    data = [{'subject': subject_label, 'session': session_label, 'age': age_in_months, 'sex': sex, 'acquisition': acquisition_cleaned , "input_gear_v": gear_v, "scannerSoftwareVersion": scannerSoftwareVersion }]  
+    data = [{'subject': subject_label, 'session': session_label, 'age': age, 'age_source': age_source, 'sex': sex, 'acquisition': acquisition_cleaned, "input_gear_v": gear_v, "scanner_software_v": scannerSoftwareVersion }]  
     # Creates DataFrame.  
     demo = pd.DataFrame(data)
+
     log.info(f"Demographics:  {subject_label} {session_label} {str(age_in_months)} {sex}")
 
     return demo
 
     # -------------------  Concatenate the data  -------------------  #
 
-def housekeeping(demo):
+def housekeeping(demographics):
 
-    acq = demo['acquisition'].values[0]
-    sub = demo['subject'].values[0]
+    acq = demographics['acquisition'].values[0]
+    sub = demographics['subject'].values[0]
 
     filePath = '/flywheel/v0/work/All_volumes.csv'
     volumes = pd.read_csv(filePath, sep='\s+', engine='python') #index_col=False,
-    df = pd.concat([demo.reset_index(drop=True), volumes.reset_index(drop=True)], axis=1)
+    df = pd.concat([demographics.reset_index(drop=True), volumes.reset_index(drop=True)], axis=1)
     out_name = f"{acq}_volumes.csv"
     outdir = ('/flywheel/v0/output/' + out_name)
     df.to_csv(outdir, index=False)
